43dadca429747bbd5444e753a897895415c1294a
Alisher <alisher22000055@mail.ru>
2022-01-27 15:09:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const answer = prompt("qwerty", '');
+// console.log(answer + '5');
+
+const answer = [];
+answer[0] = prompt('Как ваше имя?');
+console.log(`Его имя ${answer}`);
